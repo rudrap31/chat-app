fd4deb74c8dd55377371c5e77735026f3de952b4
Rudra Patel <rudrap31@students.cs.ubc.ca>
2025-02-03 22:41:02 -0800

    implemented signup

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -1,5 +1,55 @@
+import User from "../models/user.js"
+import bcrypt from "bcryptjs"
+
 export const signup = async (req, res) => {
-    res.send("signup Route");
+    try {
+        const { fullName, username, password, confirmPassword } = req.body;
+
+        if(password !== confirmPassword){
+            return res.status(400).json({ error:"Password do not match" })
+        }
+
+        const user = await User.findOne({ username });
+
+        if(user){
+            return res.status(400).json({ error:"Username already exists" })
+        }
+
+       
+        // hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const firstName = fullName.split(" ")[0];
+        const lastName = fullName.split(" ")[1];
+        const profilePic = `https://avatar.iran.liara.run/username?username=[${firstName}+${lastName}]`
+        
+        
+        const newUser = new User({
+            fullName,
+            username,
+            password: hashedPassword,
+            profilePic
+        })
+        
+        if (newUser) { 
+            // JWT
+            await newUser.save();
+            
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic,
+            });
+        } else{
+            res.status(400).json({error: "Invalid user"})
+        }
+
+    } catch (error) {
+        console.log("Error in signing up")
+        res.status(500).json({error: error.message})
+    }
 }
 
 export const login = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.js"
 import connectMongoDB from "./db/mongodb.js";
+
 const app = express();
-dotenv.config()
-
 const PORT = process.env.PORT || 5001
+dotenv.config();
 
-app.get("/", (req, res) => {
-    // root route http://localhost:5000/
-    res.send("hello");
-})
+app.use(express.json()); // to parse incoming requests
+
+// app.get("/", (req, res) => {
+//     // root route http://localhost:5000/
+//     res.send("hello");
+// })
 
 app.use("/api/auth", authRoutes);
 
